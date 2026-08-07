b8d05e4c9326f6c97d168ed8362d606f96d74678
martijnschlosser <martijnschlosser1@gmail.com>
2026-08-07 11:59:17 +0300

    Disable broken image optimizer

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  images: {
+    unoptimized: true,
+  },
   async headers() {
     return [
       {
